d27a7e8d3a80cf4b2b9a426a7d2a781b5a0d97af
Arediss <quentincattoen@hotmail.fr>
2012-06-02 23:08:24 +0200

    Ajout PHP Design

---
+++ MyCraft/bas.php
@@ -0,0 +1,5 @@
+		<div id="pied_de_page">
+			&copy;2012 Monsite.com | MineStyle par Arediss - Tous droits r&eacute;serv&eacute;s
+		</div>
+   </body>
+</html>
---
+++ MyCraft/haut.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" >
+   <head>
+       <title>Bienvenue sur mon site !</title>
+       <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+		<link rel="stylesheet" media="screen" type="text/css" title="Mécanique" href="style.css" />
+   </head>
+   <body>   
+		<div id="en_tete">
+			<div class="logo">
+				<a href="index.html"><img src="images/logo.gif" /></a>
+			</div>
+		</div>
+		<div id="menu">
+			<a class="menu" href="index.html">Accueil</a>
+			<a class="menu" href="#">Inscription</a>
+			<a class="menu" href="#">Rejoindre</a>
+			<a class="menu" href="#">Galerie</a>
+			<a class="menu" href="#">F.A.Q</a>
+			<a class="menu" href="#">Boutique</a>
+			<a class="menu" href="#">Forum</a>
+		</div>
